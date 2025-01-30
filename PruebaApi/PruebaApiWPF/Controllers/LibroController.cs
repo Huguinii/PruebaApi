35cc9d12928d7bffd6f767e95ebc2b3c73567b54
Huguinii <hugomnbn@gmail.com>
2025-01-30 13:42:35 +0100

    PruebaApi error No se puede abrir 'C:\Users\Hugo\Interfaces\PruebaApi\PruebaApi\PruebaApi\obj\Debug\net8.0\PruebaApi.dll' para escribir: 'Acceso denegado a la ruta de acceso

---
+++ PruebaApi/PruebaApiWPF/Controllers/LibroController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PruebaApiWPF.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaApiWPF.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class LibroController : ControllerBase
+    {
+        private readonly ILogger<LibroController> _logger;
+
+        private static List<LibroDTO> Libros = new List<LibroDTO>()
+        {
+            new LibroDTO()
+            {
+                ISBN="123456",
+                Id=1,
+                NumPag=200,
+                Titulo="Pinocchio"
+            },
+            new LibroDTO()
+            {
+                ISBN="654321",
+                Id=2,
+                NumPag=123,
+                Titulo="The Hunger Games"
+            }
+        };
+
+        public LibroController(ILogger<LibroController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IEnumerable<LibroDTO> Get()
+        {
+            return Libros;
+        }
+
+        [HttpGet]
+        public LibroDTO Get(int id)
+        {
+            return Libros.FirstOrDefault(x => x.Id == id);
+        }
+
+        [HttpPost]
+        public LibroDTO Post([FromBody] LibroDTO libro)
+        {
+            if (Libros.Any(x => x.Id == libro.Id))
+            {
+                return null;
+            }
+            Libros.Add(libro);
+            return libro;
+        }
+
+        [HttpPut]
+        public LibroDTO Put(LibroDTO libro,int id)
+        {
+            if (id != libro?.Id)
+            {
+                return null;
+            }
+
+            LibroDTO? libroBBDD = Libros.FirstOrDefault(x => x.Id == libro.Id);
+            if (libroBBDD == null)
+            {
+                return null;
+            }
+            libroBBDD.ISBN= libro.ISBN;
+            libroBBDD.NumPag= libro.NumPag;
+            libroBBDD.Titulo= libro.Titulo;
+
+            return libroBBDD;
+        }
+        [HttpDelete]
+        public bool Remove(int id)
+        {
+            LibroDTO? libroBBDD = Libros.FirstOrDefault(x => x.Id == id);
+            if (libroBBDD == null)
+            {
+                return false;
+            }
+            
+            return Libros.Remove(libroBBDD);
+        }
+
+    }
+}
