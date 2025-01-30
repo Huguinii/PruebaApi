8a2a5f6994ab9299c446d067beeddd292226d435
Huguinii <hugomnbn@gmail.com>
2025-01-30 15:46:02 +0100

    PeliculaDTO

---
+++ ProAPI/Models/DTOs/PeliculaDTO/CreatePeliculaDTO.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestAPI.Models.DTOs.PeliculaDTO
+{
+    public class CreatePeliculaDTO
+    {
+        [Required(ErrorMessage = "Titulo is required")]
+        [MaxLength(50, ErrorMessage = "Max char is 50")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "MinutosDuracion is required")]
+        public int MinutosDuracion { get; set; }
+
+        [Required(ErrorMessage = "Director is required")]
+        [MaxLength(50, ErrorMessage = "Max char is 50")]
+        public string Director { get; set; }
+
+        [Required(ErrorMessage = "Presupuesto is required")]
+        public double Presupuesto { get; set; }
+
+        [Required(ErrorMessage = "FechaDeEstreno is required")]
+        public DateOnly FechaDeEstreno { get; set; }
+
+    }
+}
---
+++ ProAPI/Models/DTOs/PeliculaDTO/PeliculaDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace RestAPI.Models.DTOs.PeliculaDTO
+{
+    public class PeliculaDTO : CreatePeliculaDTO
+    {
+        public string Titulo {  get; set; }
+        public int MinutosDuracion {  get; set; }
+        public string Director {  get; set; }
+        public double Presupuesto { get; set; }
+        public DateOnly FechaDeEstreno { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
